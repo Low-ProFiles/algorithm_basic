641da56ab0da45176733cea60a18eabfaa46fe85
Low-ProFiles <69390311+Low-ProFiles@users.noreply.github.com>
2021-06-06 10:32:13 +0900

    Add files via upload

---
+++ 08. 파이썬 반복문/161~170/166.py
@@ -0,0 +1,2 @@
+for i in range(10):
+    print('3 * {i} = {result}'.format(i=i, result=3*i))
---
+++ 08. 파이썬 반복문/161~170/167.py
@@ -0,0 +1,3 @@
+num = 3
+for i in range(1, 10, 2) :
+    print ('3 * {i} = {result}'.format(i = i, result = 3 *i))
