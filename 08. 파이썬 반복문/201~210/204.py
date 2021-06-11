7ed6624ea0c4cc4ab9cc9fdf164da3e82a12425f
Low-ProFiles <69390311+Low-ProFiles@users.noreply.github.com>
2021-06-12 00:39:09 +0900

    Delete 08. 파이썬 반복문/201~210 directory

--- 08. 파이썬 반복문/201~210/204.py
+++
@@ -1,4 +0,0 @@
-def print_coin():
-    for i in range(100):
-        print('비트코인')
-
--- 08. 파이썬 반복문/201~210/206.py
+++
@@ -1,9 +0,0 @@
-def message() :
-    print("A")
-    print("B")
-
-message()
-print("C")
-message()
-
-#A,B,C,A,B가 나올 것
