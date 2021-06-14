4bd26526d449cb0794ee7c5070c2f79cef879cdc
Low-ProFiles <69390311+Low-ProFiles@users.noreply.github.com>
2021-06-14 23:41:16 +0900

    Add files via upload

--- 09. 파이썬 함수/221~230/221.py
+++ 09. 파이썬 함수/221~230/221.py
@@ -1,1 +1,8 @@
-#dds
+#string 슬라이싱
+
+string = input()
+
+def print_reverse(string):
+    print(string[::-1])
+
+print_reverse(string)
---
+++ 09. 파이썬 함수/221~230/223.py
@@ -0,0 +1,6 @@
+def print_even(elements):
+    for v in elements:
+        if v % 2 == 0:
+            print(v)
+
+print(print_even([1, 3, 2, 10, 12, 11, 15]))
